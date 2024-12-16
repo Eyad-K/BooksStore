11c49b6e4871cef697cee64cf8a34523f86a13de
Eyad-K <111588558+Eyad-K@users.noreply.github.com>
2024-12-16 16:51:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// دعم الملفات الثابتة
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    DefaultFileNames = new List<string> { "home.html" }
+});
+app.UseStaticFiles();
+
+app.Run();
